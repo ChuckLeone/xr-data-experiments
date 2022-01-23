1be3bf3fd691b97e71a95f09e99ce6236916db96
Chuck Leone <chuckleone@gmail.com>
2022-01-23 16:05:58 -0500

    camera and grid
    Add three Drei for camera and orbit controls. added perspective grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import data from "../data/mockup1.js";
 import { VRButton } from "three/examples/jsm/webxr/VRButton.js";
 import { WebGL1Renderer } from "three/src/renderers/WebGL1Renderer.js";
+import { Center, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
 function Cube(props) {
   const ref = useRef();
@@ -14,28 +15,37 @@ function Cube(props) {
   useFrame((state, delta) => (ref.current.rotation.x = 0));
 
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry args={[0.1, 0.1, 0.1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "#20639b"} />
-    </mesh>
+    <>
+      <mesh
+        {...props}
+        ref={ref}
+        scale={clicked ? 1.5 : 1}
+        onClick={(event) => click(!clicked)}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+      >
+        <boxGeometry args={[0.1, 0.1, 0.1]} />
+        <meshStandardMaterial color={hovered ? "hotpink" : "#20639b"} />
+      </mesh>
+    </>
   );
 }
 
 function Home(props) {
   return ReactDOM.render(
     <Canvas style={{ backgroundColor: "gray", height: "100vh" }}>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      {data.map((item) => (
-        <Cube key={item.id} position={[item.id - 1.5, 0, 0]} />
-      ))}
+      <PerspectiveCamera makeDefault position={[0, 0, 10]} />
+      <group position={[0, 0, -10]}>
+        <gridHelper args={[100, 100]} />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Center position={[5, 5, 10]}>
+          {data.map((item) => (
+            <Cube key={item.id} position={[item.id - 1.5, 0, 0]} />
+          ))}
+        </Center>
+      </group>
+      <OrbitControls />
     </Canvas>,
     document.getElementById("root")
   );
